307ea6776f188daf9f173bfd8ae88a82e9f1d74a
Barnabas Bucsy <barnabas.bucsy@gmail.com>
2021-05-19 13:01:21 +0200

    Initial commit

--- configuration/.platform/hooks/postdeploy/00-aws-eb-vars.sh
+++ configuration/.platform/hooks/postdeploy/00-aws-eb-vars.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
+# WARN: always ensure line endings are set to LF!
 
 AWS_REGION=$(cat /opt/elasticbeanstalk/config/ebenvinfo/region)
 AWS_ENV_ID=$(cat /opt/elasticbeanstalk/config/ebenvinfo/envid)
 IMDS_TOKEN=$(curl --silent --max-time 60 -X PUT http://169.254.169.254/latest/api/token -H "X-aws-ec2-metadata-token-ttl-seconds: 30")
 AWS_EC2_ID=$(curl --silent --max-time 60 -H "X-aws-ec2-metadata-token: $IMDS_TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
+chmod 666 /opt/elasticbeanstalk/deployment/env
 cat <<EOT>> /opt/elasticbeanstalk/deployment/env
 AWS_REGION=$AWS_REGION
 AWS_ENV_ID=$AWS_ENV_ID
 AWS_EC2_ID=$AWS_EC2_ID
 EOT
+chmod 400 /opt/elasticbeanstalk/deployment/env
+systemctl restart web.service
